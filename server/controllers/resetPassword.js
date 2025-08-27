ce2f2fbcdc64a57a64885d05f6b500af9e826cd2
sonu kumar <124373321+sonukumar6343@users.noreply.github.com>
2025-08-27 19:28:19 +0530

    Update resetPassword.js

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -26,7 +26,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://learn-kode-frontend.vercel.app/update-password/${token}`
+    const url = `https://learn-kode.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
